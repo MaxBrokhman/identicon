34d0e715048e109a16192d467c50a25d5368cf8c
Max Brokhman <maxbrokhman@gmail.com>
2022-11-30 07:12:04 +0200

    convertion string to hex added

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,20 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
+  def main(input) do
+    input
+      |> hash_input
+      |> pick_color
 
-  @doc """
-  Hello world.
+  end
 
-  ## Examples
+  def pick_color(image) do
+    %Identicon.Image{hex: [red, green, blue | _tail]} = image
+    %Identicon.Image{image | color: {red, green, blue}}
+  end
 
-      iex> Identicon.hello()
-      :world
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
 
-  """
-  def hello do
-    :world
+    %Identicon.Image{hex: hex}
   end
 end
