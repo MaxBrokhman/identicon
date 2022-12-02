98f6cdf46ae7ef35a786d17d87d5c199ada743dd
Max Brokhman <maxbrokhman@gmail.com>
2022-12-02 10:00:29 +0200

    grid building

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,7 +3,31 @@ defmodule Identicon do
     input
       |> hash_input
       |> pick_color
+      |> build_grid
+      |> filter_odd_squares
+  end
 
+  def filter_odd_squares(image) do
+    %Identicon.Image{grid: grid} = image
+   updatedGrid = Enum.filter(grid, fn({code, _idx}) ->
+      rem(code, 2) == 0
+    end)
+    %Identicon.Image{image | grid: updatedGrid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+    grid = hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
   end
 
   def pick_color(image) do
